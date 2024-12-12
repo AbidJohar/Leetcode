c00bbb0fdcae3bcfbbdc4073d71b57af3ec8b600
Javascript_Developer <ah1502651@gmail.com>
2024-12-13 02:29:10 +0500

    3sum solution

--- Medium/3sum.cpp
+++ Medium/3sum.cpp
@@ -1,34 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> threeSum(vector<int>& nums) {
+vector<vector<int>> threeSum(vector<int> &nums)
+{
     vector<vector<int>> results;
-    vector<vector<int>> threeSum;
-    for (int i = 0; i < nums.size(); i++) {
-        for(int j=0; j<nums.size(); j++ ){
-            for(int k=0; j<nums.size(); j++){
-                if(nums[i] + nums[j] + nums[k] == 0){
-                    results.push_back({nums[i],nums[j],nums[k]});
-                    k++;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int right = nums.size() - 1;
+        int left = i + 1;
+        if (i > 0 && nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+
+        while (left < right)
+        {
+
+            if (nums[right] + nums[left] + nums[i] == 0)
+            {
+                results.push_back({nums[i], nums[left], nums[right]});
+                left++;
+                while (nums[left] == nums[left - 1] && left < right)
+                {
+                    left++;
                 }
             }
-                j++;
+            if (nums[right] + nums[left] + nums[i] > 0)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
         }
-                i++;
-        }
+    }
+
     return results;
 }
 
-int main() {
-    vector<int> numbers = {-1,0,1,2,-1,-4};
+int main()
+{
+    vector<int> numbers = {-1, 0, 1, 0};
     vector<vector<int>> results = threeSum(numbers);
 
     // Print the results
-    for (const auto& triplet : results) {
+    for (const auto &triplet : results)
+    {
         cout << "[";
-        for (size_t i = 0; i < triplet.size(); i++) {
+        for (size_t i = 0; i < triplet.size(); i++)
+        {
             cout << triplet[i];
-            if (i < triplet.size() - 1) cout << ", ";
+            if (i < triplet.size() - 1)
+                cout << ", ";
         }
         cout << "] ";
     }
